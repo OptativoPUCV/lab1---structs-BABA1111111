b4b427dec408130b500cf47d68b70c9e1139e8b1
Your Name <you@example.com>
2024-03-16 19:51:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -71,7 +71,7 @@ arreglos en un tercer arreglo también ordenado.
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
                        int result[]) {
 
-  newSize = size1 + size2;
+  // newSize = size1 + size2;
   
                        }
 
